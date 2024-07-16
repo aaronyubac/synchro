f5899e52bf43c0bedee34074880858a14448f550
Aaron Anthony Yubac <aaronyubac@gmail.com>
2024-07-16 06:51:39 -0700

    unavailability to bar

--- ui/static/js/timebar.js
+++ ui/static/js/timebar.js
@@ -1,5 +1,5 @@
-const timebarGridAM = document.querySelector(".timebar.am .timebar-grid");
-const timebarGridPM = document.querySelector(".timebar.pm .timebar-grid");
+const timebarGridAM = document.querySelectorAll(".timebar .am .timebar-grid");
+const timebarGridPM = document.querySelectorAll(".timebar .pm .timebar-grid");
 
 let timeslotsAM = "";
 let timeslotsPM = "";
@@ -9,24 +9,20 @@ function renderTimeslots() {
   for (let i = 1; i < 49; i++) {
     if (i === 1) {
       timeslotsAM += `<div class="time-slot" data-time="15">
-                            <div class="time-box"></div>
                             <div class="time-marker time-marker-start">12:00AM</div>
                         </div>`;
     } else if (i === 48) {
       timeslotsAM += `<div class="time-slot" data-time="720">
-                                <div class="time-box"></div>
                                 <div class="time-marker">12:00PM</div>
                             </div>`;
     } else if ((i * 15) % 60 === 0) {
       timeslotsAM += `<div class="time-slot" data-time="${i * 15}">
-                                <div class="time-box"></div>
                                 <div class="time-marker">${
                                   (i * 15) / 60
                                 }:00AM</div>
                             </div>`;
     } else {
       timeslotsAM += `<div class="time-slot" data-time="${i * 15}">
-                            <div class="time-box"></div>
                             </div>`;
     }
   }
@@ -36,78 +32,84 @@ function renderTimeslots() {
   for (let i = 49; i < 97; i++) {
     if (i === 49) {
       timeslotsPM += `<div class="time-slot" data-time="735">
-                                <div class="time-box"></div>
                                 <div class="time-marker time-marker-start">12:00PM</div>
                             </div>`;
     } else if (i === 96) {
       timeslotsPM += `<div class="time-slot" data-time="1440">
-                                <div class="time-box"></div>
                                 <div class="time-marker">12:00AM</div>
                             </div>`;
     } else if ((i * 15) % 60 === 0) {
       timeslotsPM += `<div class="time-slot" data-time="${i * 15}">
-                                <div class="time-box"></div>
                                 <div class="time-marker">${
-                                  (i * 15) / 60
+                                  ((i * 15) / 60) % 12
                                 }:00PM</div>
                             </div>`;
     } else {
       timeslotsPM += `<div class="time-slot" data-time="${i * 15}">
-                                <div class="time-box"></div>
                             </div>`;
     }
   }
 
-  timebarGridAM.innerHTML = timeslotsAM;
-  timebarGridPM.innerHTML = timeslotsPM;
+  timebarGridAM[0].innerHTML = timeslotsAM;
+  timebarGridPM[0].innerHTML = timeslotsPM;
+  timebarGridAM[1].innerHTML = timeslotsAM;
+  timebarGridPM[1].innerHTML = timeslotsPM;
 }
 
 function unavailabilitiesToBar() {
-  
   let current = document.querySelector(".days .day.active");
   if (current != null) {
-    let unavailabilityData = JSON.parse(current.dataset.eventUnavailabilities);
+    let eventUnavailabilities = JSON.parse(current.dataset.eventUnavailabilities);
+    let userUnavailabilities = JSON.parse(current.dataset.userUnavailabilities);
+
+    const unavailabilityData = [userUnavailabilities, eventUnavailabilities];
+
+    let timeboxes = [];
+    let userTab = Boolean;
     
-    let timeboxes = document.querySelectorAll(".time-slot[data-time] .time-box");
+    for (let i = 0; i < 2; i++) {
+      
+      if (i === 0) {
+        userTab = true;
+        timeboxes = document.querySelectorAll(".user .time-slot[data-time]");
+      } else if (i === 1) {
+        userTab = false;
+        timeboxes = document.querySelectorAll(".event .time-slot[data-time]");
+      }
 
-    timeboxes.forEach(timebox => {
-      timebox.classList = "time-box";
-    });
+      timeboxes.forEach((timebox) => {
+        timebox.classList = "time-slot";
+      });
 
-    unavailabilityData.forEach((unavailability) => {
-      if (unavailability.allDay === "true") {
+      unavailabilityData[i].forEach((unavailability) => {
+        if (unavailability.allDay === "true") {
 
-
-        timeboxes.forEach(timebox => {
+          timeboxes.forEach((timebox) => {
             timebox.classList.add("allDay");
-        });
+          });
 
+        } else {
 
-      } else {
-        // convert start and end to minutes
-        const options = {
-          hour: "numeric",
-          minute: "numeric",
-        };
+          startTime = new Date(unavailability.start);
+          endTime = new Date(unavailability.end);
 
-        startTime = new Date(unavailability.start);
-        endTime = new Date(unavailability.end);
+          startAsMinutes = startTime.getHours() * 60 + startTime.getMinutes();
+          endAsMinutes = endTime.getHours() * 60 + endTime.getMinutes();
 
-        startAsMinutes = (startTime.getHours() * 60) + startTime.getMinutes();
-        endAsMinutes = (endTime.getHours() * 60) + endTime.getMinutes();
+          for (let i = startAsMinutes + 15; i < endAsMinutes + 15; i += 15) {
+            let currentTimebox;
+            if (userTab) {
+              currentTimebox = document.querySelector(`.user .time-slot[data-time="${i}"]`);
+            } else {
+              currentTimebox = document.querySelector(`.event .time-slot[data-time="${i}"]`);
+            }
 
-
-        for (let i = (startAsMinutes + 15); i < (endAsMinutes + 15); i += 15) {
-            let currentTimebox = document.querySelector(`.time-slot[data-time="${i}"] .time-box`);
-
-            currentTimebox.classList.add("partial");
+            if (currentTimebox != null) {
+              currentTimebox.classList.add("partial");
+            }
+          }
         }
-
+      });
     }
-    
-
-    });
   }
-
-  //     // keep changing until end time(converted) matches data-time
 }
